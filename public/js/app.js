0d97e3aed25d175f76e0f451ab4daae56a4fdbd7
Kiran gavini <kirangavini1993@gmail.com>
2017-02-06 15:16:28 -0600

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,11 @@ var socket = io();
 
 socket.on('connect', function () {
 	console.log('connected to io.server');
-})
+});
+
+socket.on('message', function (message) {
+   console.log('New message:');
+   console.log(message.text);
+
+});
+
