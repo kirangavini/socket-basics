02c394ec92423b48682f7a2cbde3444b2f7fcf4f
Kiran gavini <kirangavini1993@gmail.com>
2017-02-08 12:38:24 -0600

    add message to browser UI

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ socket.on('message', function (message) {
    console.log('New message:');
    console.log(message.text);
 
-   // jQuery('.messages').append('<p>'+ message.text +'</p>');
+    jQuery('.messages').append('<p>'+ message.text +'</p>');
 
 });
 
